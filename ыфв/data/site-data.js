a852e820bc0f941ced1b74798ccad124cc0a982d
aleshaqqe <leshatk765@gmail.com>
2026-04-08 17:41:43 +0300

    test

--- ыфв/data/site-data.js
+++
@@ -1,9 +0,0 @@
-const { SUPPORTED_LANGS, DEFAULT_LANG, UI_TRANSLATIONS } = require("./translations");
-const { GAMES } = require("./games");
-
-module.exports = {
-  SUPPORTED_LANGS,
-  DEFAULT_LANG,
-  UI_TRANSLATIONS,
-  GAMES
-};
